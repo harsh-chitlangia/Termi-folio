9a19abe4de6d3b00754afc2e6c743a23ea2f4a48
harsh-chitlangia <105810385+harsh-chitlangia@users.noreply.github.com>
2022-05-18 22:33:26 +0530

    Add files via upload

---
+++ Termi-folio/data.js
@@ -0,0 +1,18 @@
+var data = {
+  name: [
+    { name: 'Name', value: 'Harsh Chitlangia' }
+  ],
+  bio: [
+    { name: 'Name', value: 'Harsh Chitlangia' },
+    { name: 'Email', value: 'harshchitlangia77@gmail.com' },
+    { name: 'Phone', value: '1234567890' },
+    { name: 'Country', value: 'India' },
+    { name: 'State', value: 'Rajasthan' },
+    { name: 'City', value: 'Sikar' }
+  ],
+  socials: [
+    { name: 'OpenForge', value: 'https://openforge.gov.in/harshchitlangia' },
+    { name: 'GitHub', value: 'https://github.com/harsh-chitlangia' },
+    { name: 'Koo', value: 'https://www.kooapp.com/profile/harshchitlangia' },
+  ]
+}
